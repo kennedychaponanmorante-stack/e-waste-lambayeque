929a3ca66fd395ac7bcc6908d4cd091828b9a803
kennedychaponanmorante-stack <kennedychaponanmorante@gmail.com>
2026-02-17 09:47:38 -0500

    Fix: Conexión de bóton de comentarios a Supabase x2

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,3 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = 'https://ldurxuoowyiprwdpoore.supabase.co' 
 const supabaseAnonKey = 'sb_publishable_erRH5sGjsv6GDDziRoEUqA_1yNR7QuT' 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
