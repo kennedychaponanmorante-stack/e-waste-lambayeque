b436e9cc1136eb19f2f59d59298a8c9ccffc04c8
kennedychaponanmorante-stack <kennedychaponanmorante@gmail.com>
2026-02-17 05:29:15 -0500

    Carga final del proyecto e-waste

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hola Mundo c:")
+
+numero1 = 4
+numero2 = 28
+resultado = numero1 + numero2 
+
+print("la suma de numero1 y numero 2 es", resultado)
---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseUrl = 'https://ldurxuoowyiprwdpoore.supabase.co' 
+const supabaseAnonKey = 'sb_publishable_erRH5sGjsv6GDDziRoEUqA_1yNR7QuT' 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
